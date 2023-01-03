1031b70b75e981f7e71771d06aa716dc0121f2e4
Paavan Showri <118935216+paavan-showri@users.noreply.github.com>
2023-01-04 00:01:38 +0530

    Update SQL Joins.sql

--- SQL Joins.sql
+++ SQL Joins.sql
@@ -30,7 +30,7 @@ ON region.id=sales_reps.region_id
 JOIN orders
 ON accounts.id = orders.account_id;
 
---Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for the Midwest region. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to account name./*
+--Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for the Midwest region. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to account name.
 
 SELECT region.name r_name,sales_reps.name s_name,accounts.name a_name 
 FROM sales_reps
@@ -63,7 +63,7 @@ ON region.id=sales_reps.region_id
 JOIN orders
 ON accounts.id = orders.account_id AND orders.standard_qty > 100
 
---Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order. However, you should only provide the results if the standard order quantity exceeds 100 and the poster order quantity exceeds 50. Your final table should have 3 columns: region name, account name, and unit price. Sort for the smallest unit price first. In order to avoid a division by zero error, adding .01 to the denominator here is helpful (total_amt_usd/(total+0.01)./*
+--Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order. However, you should only provide the results if the standard order quantity exceeds 100 and the poster order quantity exceeds 50. Your final table should have 3 columns: region name, account name, and unit price. Sort for the smallest unit price first. In order to avoid a division by zero error, adding .01 to the denominator here is helpful (total_amt_usd/(total+0.01).
 
 SELECT region.name r_name,accounts.name as a_name,(total_amt_usd)/(total+0.01) as unit_price
 FROM accounts
@@ -74,12 +74,12 @@ ON region.id=sales_reps.region_id
 JOIN orders
 ON accounts.id = orders.account_id AND orders.standard_qty > 100 AND poster_qty >50 ORDER BY unit_price;
 
---What are the different channels used by account id 1001? Your final table should have only 2 columns: account name and the different channels. You can try SELECT DISTINCT to narrow down the results to only the unique values./*
+--What are the different channels used by account id 1001? Your final table should have only 2 columns: account name and the different channels. You can try SELECT DISTINCT to narrow down the results to only the unique values.
 
 SELECT DISTINCT channel,account_id from web_events
 WHERE account_id= 1001;
 
---Find all the orders that occurred in 2015. Your final table should have 4 columns: occurred_at, account name, order total, and order total_amt_usd./*
+--Find all the orders that occurred in 2015. Your final table should have 4 columns: occurred_at, account name, order total, and order total_amt_usd.
 
 SELECT orders.occurred_at, orders.total,orders.total_amt_usd,accounts.name
 FROM orders
